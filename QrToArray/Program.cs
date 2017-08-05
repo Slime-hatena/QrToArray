1f59ed96e6c338b54a25221372190c9345332bd4
Slime_hatena <Slime-hatena@users.noreply.github.com>
2017-08-06 01:30:43 +0900

    add files

---
+++ QrToArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace QrToArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Drawing.Bitmap bitmap;
+
+			bitmap = new Bitmap("local.png");
+			stream.Close();
+        }
+    }
+}
